a14050fc4e23f6a461e9b440b9232fdc562b6737
LuizH-Angelo <luizh.alv@gmail.com>
2026-03-26 20:13:41 -0300

    Add files via upload

--- src/components/Profissionais.jsx
+++ src/components/Profissionais.jsx
@@ -7,40 +7,40 @@ function Profissionais() {
             <div className='s2divTitle'>
                 <h2 className='s2Title'>Profissionais</h2>
             </div>
-            
+
             <section className='s2' id='s2'>
-                
-                <div className="profs">
+
+                {/* <div className="profs">
                     <img src={profsTeste} alt="professores da Angelos" className='imgprofs'/>
-                </div>
+                </div> */}
 
                 <div className="s2card">
-                    <h3 className='s2cardTitle'>Saulo Minato</h3>
-                    <p className='s2cardContent'>Professor VETERANO da Angelo's! Conosco desde o início, especializado em ...</p>
-                </div>
-
-                <div className="profs">
-                    <img src={profsTeste} alt="professores da Angelos" className='imgprofs'/>
-                </div>
-                <div className="s2card">
-                    <h3 className='s2cardTitle'>Nome 2</h3>
-                    <p className='s2cardContent'>Texto 2</p>
-                </div>
-
-                <div className="profs">
-                    <img src={profsTeste} alt="professores da Angelos" className='imgprofs'/>
+                    <img src={profsTeste} alt="" className='profs' />
+                    <div className="s2card__content">
+                        <p className="s2card__title">Card Title</p>
+                        <p className="s2card__description">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco.</p>
+                    </div>
                 </div>
                 <div className="s2card">
-                    <h3 className='s2cardTitle'>Nome 3</h3>
-                    <p className='s2cardContent'>Texto 3</p>
-                </div>
-
-                <div className="profs">
-                    <img src={profsTeste} alt="professores da Angelos" className='imgprofs'/>
+                    <img src={profsTeste} alt="" className='profs' />
+                    <div className="s2card__content">
+                        <p className="s2card__title">Card Title</p>
+                        <p className="s2card__description">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco.</p>
+                    </div>
                 </div>
                 <div className="s2card">
-                    <h3 className='s2cardTitle'>Nome 4</h3>
-                    <p className='s2cardContent'>Texto 4</p>
+                    <img src={profsTeste} alt="" className='profs' />
+                    <div className="s2card__content">
+                        <p className="s2card__title">Card Title</p>
+                        <p className="s2card__description">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco.</p>
+                    </div>
+                </div>
+                <div className="s2card">
+                    <img src={profsTeste} alt="" className='profs' />
+                    <div className="s2card__content">
+                        <p className="s2card__title">Card Title</p>
+                        <p className="s2card__description">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco.</p>
+                    </div>
                 </div>
 
             </section >
